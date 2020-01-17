33a9c0c3758f948435d6c4587fa4a7be94bdebf6
Otoon <oliviernguyenyb@gmail.com>
2020-01-17 23:23:29 +0100

    Add player and movement

---
+++ Assets/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") ) * 50;
+        if (movementVector.sqrMagnitude >= 1)
+            movementVector.Normalize();
+        myRigidBody.velocity = movementVector * speed;
+
+    }
+}
