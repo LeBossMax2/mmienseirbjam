2955a0fd1212344133fe66139a931c3f488a28b1
Lucas Guedon <guedon.lucas@laposte.net>
2020-01-18 15:40:32 +0100

    Add simple Main menu

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadSceneAsync("Sandbox");
+    }
+
+    public void showHowToPlay()
+    {
+
+    }
+
+    public void showOptions()
+    {
+
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
